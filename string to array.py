c0d22629481d194bf00da7b37207a500d74a8f0a
amit <akum2018@gmail.com>
2023-04-21 00:46:05 +0530

    german has done

--- string to array.py
+++ string to array.py
@@ -1,4 +1,23 @@
-a = ''''''
+a = '''Wie geht es dir? - How are you?
+Mir geht es gut, danke. - I'm fine, thank you.
+Ich spreche kein Deutsch. - I don't speak German.
+Kannst du das bitte wiederholen? - Can you please repeat that?
+Ich verstehe nicht. - I don't understand.
+Wo ist die Toilette? - Where is the toilet?
+Was ist das? - What is that?
+Ich heiße (Name). - My name is (Name).
+Wie spät ist es? - What time is it?
+Entschuldigung - Excuse me/I'm sorry.
+Ich komme aus Deutschland. - I'm from Germany.
+Ich lerne Deutsch. - I'm learning German.
+Was möchtest du trinken? - What would you like to drink?
+Kannst du mir helfen, bitte? - Can you help me, please?
+Ich liebe dich. - I love you.
+Das ist fantastisch! - That's fantastic!
+Ich bin müde. - I'm tired.
+Wo ist der Bahnhof? - Where is the train station?
+Ich habe eine Frage. - I have a question.
+Wie heißt das auf Deutsch? - What is that called in German?'''
 
 z = []
 y = []
