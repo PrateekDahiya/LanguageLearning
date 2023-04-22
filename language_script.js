0820e8392e421cdc92aabcd9507d35a233fc6fcd
amit <akum2018@gmail.com>
2023-04-22 11:14:33 +0530

    Done some CSS

--- language_script.js
+++ language_script.js
@@ -4,9 +4,16 @@ function speak(b) {
   msg.lang = b;
   window.speechSynthesis.speak(msg);
 }
+function speakeng() {
+  var msg = new SpeechSynthesisUtterance(english[i]);
+  msg.lang = 'en-US';
+  window.speechSynthesis.speak(msg);
+}
 function nextword() {
-  i = i + 1;
-  document.getElementById("lang").innerHTML = otherlang[i];
-  document.getElementById("eng").innerHTML = english[i];
+  if (i < english.length - 1) {
+    i = i + 1;
+    document.getElementById("lang").innerHTML = otherlang[i];
+    document.getElementById("eng").innerHTML = english[i];
+  }
 }
 
